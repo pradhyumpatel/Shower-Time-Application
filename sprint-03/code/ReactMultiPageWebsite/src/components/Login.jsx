d25ec0ceb2f0e109cff36a1e80ce5f89b61ec044
Pradhyum Patel <pradhyumpatel@gmail.com>
2021-04-20 00:05:03 -0500

    add mysql connection

--- sprint-03/code/ReactMultiPageWebsite/src/components/Login.jsx
+++ sprint-03/code/ReactMultiPageWebsite/src/components/Login.jsx
@@ -7,7 +7,7 @@ function Login() {
           <div class="login">
               <h1>Login Form</h1>
           <form action="http://localhost:3001/auth" method="POST">
-          <input type="text" name="username" placeholder="Username" required />
+                  <input type="text" name="username" placeholder="Username" required />
                   <input type="password" name="password" placeholder="Password" required />
                   <input type="submit" />
           </form>
