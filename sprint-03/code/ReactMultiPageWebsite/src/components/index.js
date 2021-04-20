60b598a505d63915a893bd65ac74d06e2e3d04ee
Pradhyum Patel <pradhyumpatel@gmail.com>
2021-04-20 18:23:27 -0500

    removed the reactmultipage, and add nodelogin

--- sprint-03/code/ReactMultiPageWebsite/src/components/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Navigation } from "./Navigation";
-export { default as Footer } from "./Footer";
-export { default as Home } from "./Home";
-export { default as About } from "./About";
-export { default as Timer } from "./Timer";
-export { default as Login } from "./Login";
