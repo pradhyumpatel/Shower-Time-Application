ce8cb259c5c15a234b1568417cd844c9c2c9d9d0
Pradhyum Patel <pradhyumpatel@gmail.com>
2021-05-03 20:48:46 -0500

    revert back to original

--- sprint-03/code/NodeLogin/login.js
+++ sprint-03/code/NodeLogin/login.js
@@ -29,7 +29,7 @@ app.get('/login', function(request, response) {
 	response.sendFile(path.join(__dirname + '/login.html'));
 });
 app.get('/timer', function(request, response) {
-	response.sendFile(path.join(__dirname + '/assets/js/timer.js'));
+	response.sendFile(path.join(__dirname + '/timer.html'));
 });
 
 
